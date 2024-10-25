c09e78f1522596c3cf7f484503250b0c023a1cc5
Paul Bailey <paul@neutron.studio>
2024-10-25 07:51:32 -0500

    server setup

---
+++ server/tmom/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for tmom project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmom.settings')
+
+application = get_asgi_application()
