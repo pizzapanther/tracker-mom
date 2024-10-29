512fb82734a24929623e9e79700072c5ca1d6abd
Paul Bailey <paul@neutron.studio>
2024-10-28 20:36:34 -0500

    auth setup

--- server/tmom/asgi.py
+++ server/tmom/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmom.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tmom.settings")
 
 application = get_asgi_application()
