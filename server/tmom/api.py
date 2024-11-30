683ea056205b4113c9d4a5ebb6e2e9f133e48a96
Paul Bailey <paul@neutron.studio>
2024-11-30 10:27:31 -0600

    auth done

--- server/tmom/api.py
+++ server/tmom/api.py
@@ -11,24 +11,22 @@ from tmom.exchange.api import router as exchange_router
 
 class AllAuthHeadless(SessionAuth):
   def authenticate(self, request, key=None):
-    if 'X-Session-Token' in request.headers:
-      strategy = app_settings.TOKEN_STRATEGY
-      session_token = strategy.get_session_token(request)
-      if session_token:
-        session = strategy.lookup_session(session_token)
+    strategy = app_settings.TOKEN_STRATEGY
+    session_token = strategy.get_session_token(request)
+    if session_token:
+      session = strategy.lookup_session(session_token)
+      if session:
+        user_id = session.get('_auth_user_id', None)
+        backend_path = session.get('_auth_user_backend', None)
+        if user_id and backend_path in settings.AUTHENTICATION_BACKENDS:
+          backend = load_backend(backend_path)
+          user = backend.get_user(user_id)
 
-        if session:
-          user_id = session.get('_auth_user_id', None)
-          backend_path = session.get('_auth_user_backend', None)
-          if user_id and backend_path in settings.AUTHENTICATION_BACKENDS:
-            backend = load_backend(backend_path)
-            user = backend.get_user(user_id)
-
-            if user.is_authenticated:
-              purge_request_user_cache(request)
-              request.user = user
-              request.api_session = session
-              return user
+          if user.is_authenticated:
+            purge_request_user_cache(request)
+            request.user = user
+            request.api_session = session
+            return user
 
     if request.user.is_authenticated:
       return request.user
