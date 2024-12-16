ce33851e45032b22409ce11fbccc8b22d67eeac2
Paul Bailey <paul@neutron.studio>
2024-12-16 06:23:21 -0600

    invites

---
+++ app/components/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-card class="text-negative" flat v-if="error">
+    <q-card-section>
+      <h3>{{ error }}</h3>
+    </q-card-section>
+  </q-card>
+</template>
+<script>
+export default {
+  props: ["error"],
+  setup() {},
+};
+</script>
--- app/follow/invite.vue
+++ app/follow/invite.vue
@@ -1,30 +1,43 @@
 <template>
   <tmom-title>Invite a Follower</tmom-title>
   <br />
-  <q-btn color="primary" @click="new_invite"> Create a New Invite </q-btn>
+  <div v-if="invite"></div>
+  <q-btn v-else color="primary" @click="new_invite">
+    Create a New Invite
+  </q-btn>
+  <error-banner :error="error"></error-banner>
 </template>
 <script>
+import { ref } from "vue";
+
 import API from "@/api.js";
 import EMachine from "@/encrypt.js";
 
+import ErrorBanner from "@/components/error.vue";
+
 export default {
+  components: { ErrorBanner },
   setup() {
     var api = new API();
+    var invite = ref(null);
+    var error = ref(null);
 
     function new_invite() {
       var emachine = new EMachine();
+      error.value = null;
 
       api
         .create_invite(emachine.pubkey)
         .then((resp) => {
-          console.log(resp.data);
+          invite.value = resp.data;
         })
         .catch((e) => {
           console.error(e);
+          error.value = "Error Creating Invite";
         });
     }
 
-    return { new_invite };
+    return { new_invite, error, invite };
   },
 };
 </script>
